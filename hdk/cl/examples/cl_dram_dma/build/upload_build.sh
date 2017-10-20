dd15a660a781d29bc3d697e8038d3ff8bac0f8f6
Cloud User <jamesjoethomas@gmail.com>
2017-10-20 17:46:47 +0000

    new afi

--- hdk/cl/examples/cl_dram_dma/build/upload_build.sh
+++ hdk/cl/examples/cl_dram_dma/build/upload_build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-aws s3 cp $CL_DIR/build/checkpoints/to_aws/$1 s3://fpga-dcp/dcp/
-aws ec2 create-fpga-image --region us-east-1 --name $2 --input-storage-location Bucket=fpga-dcp,Key=dcp/$1 --logs-storage-location Bucket=fpga-dcp,Key=logs
-rm $CL_DIR/build/checkpoints/to_aws/*.dcp
-rm $CL_DIR/build/checkpoints/*.dcp
-rm $CL_DIR/build/checkpoints/to_aws/*.tar
+if [ -z "$1" ]
+then
+  echo "timestamp not supplied"
+  exit
+fi
+if [ -z "$2" ]
+then
+  echo "bitstream name not supplied"
+  exit
+fi
+aws s3 cp $CL_DIR/build/checkpoints/to_aws/$1.Developer_CL.tar s3://fpga-dcp/dcp/
+aws ec2 create-fpga-image --region us-east-1 --name $2 --input-storage-location Bucket=fpga-dcp,Key=dcp/$1.Developer_CL.tar --logs-storage-location Bucket=fpga-dcp,Key=logs
+rm $CL_DIR/build/checkpoints/to_aws/$1*.dcp
+rm $CL_DIR/build/checkpoints/$1*.dcp
+rm $CL_DIR/build/checkpoints/to_aws/$1*.tar
